70b55d336ae4617c78dd463d50d42bcb2b6df37f
arnausens <arnaust3n11@gmail.com>
2022-02-10 17:41:20 +0100

    agregadas las url de las webs

--- src/components/skills.js
+++ src/components/skills.js
@@ -14,27 +14,27 @@ const Skills = () => {
         <div className='skills-wrapper'>
             <div className='skill'>
                 <h1>Web Wordpress</h1>
-                <a href=""><img src={WebTurotulo}/></a>
+                <a href="https://turotulo.es/"><img src={WebTurotulo}/></a>
                 <p>Web hecha con wordpress y Divi, en ella también hay unos ejemplos más</p>
             </div>
             <div className='skill'>
                 <h1>Web Búsqueda</h1>
-                <a href=""><img src={SearchPage}/></a>
+                <a href="https://aesquerreg-bottega-dailymarty.herokuapp.com/"><img src={SearchPage}/></a>
                 <p>Web de búsqueda, React, Redux...</p>
             </div>
             <div className='skill'>
                 <h1>Ecommerce project</h1>
-                <a href=""><img src={EcommercePage}/></a>
+                <a href="https://front-end-react-redux-ecom.herokuapp.com/"><img src={EcommercePage}/></a>
                 <p>Proyecto de Ecommerce, React, Redux...</p>
             </div>
             <div className='skill'>
                 <h1>Libros interactivos</h1>
-                <a href=""><img src={Funnybooks}/></a>
+                <a href="https://funnybooks.herokuapp.com/"><img src={Funnybooks}/></a>
                 <p>Proyecto en proceso de web de libros interactivos,<br/>React, Mobx, para el Back, Python, Django...</p>
             </div>
             <div className='skill'>
                 <h1>Portafolio project</h1>
-                <a href=""><img src={PortfolioWeb}/></a>
+                <a href="https://aeg-bottega-react-portfolio.herokuapp.com/"><img src={PortfolioWeb}/></a>
                 <p>Proyecto de portafolio, React...</p>
             </div>
 
